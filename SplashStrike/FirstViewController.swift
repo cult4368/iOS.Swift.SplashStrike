921dd5d4529aa4a8440deceeb9885e6fd52a5bba
Hong Sangho <cult4368@gmail.com>
2018-01-28 16:21:02 +0900

    Initial Commit

---
+++ SplashStrike/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SplashStrike
+//
+//  Created by Hong, Sang Ho (홍상호) on 2018. 1. 28..
+//  Copyright © 2018년 Hong, Sang Ho (홍상호). All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
